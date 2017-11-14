1375790da0e7a2b67e836b27e2fa20223fe66413
taabrde2 <31621723+taabrde2@users.noreply.github.com>
2017-11-14 16:22:23 +0100

    Erste schritte, beim erfassen von Kommentaren.

--- entries_member_details.php
+++ entries_member_details.php
@@ -6,6 +6,8 @@
     header('Location: index.php?function=login&bid='.$blogId);
   }
 
+$Comments = getComments($EntryId);
+
 $Entry = getEntry($EntryId);
 
 
@@ -33,18 +35,32 @@ echo '<p/>
     </div>
   </div>';
 
+if(!empty($_POST['title']) & !empty($_POST['content'])){
+  // hier kommt noch die Funktion zum erfassen eines Kommentares hin. 
+
+
+
+}
+
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blog-Beiträge und der Ausgabe mit PHP ersetzt werden
 ?>
-
-<div class="Kommentare">
-  <h4>Kommentare</h4>
-<ul>
-  <?php
-  foreach(){
-    
-  }
-  ?>
-</ul>
-
-
-</div>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']."?function=entries_member_create&bid=".$_SESSION['uid']."&eid=".$EntryId;?>">
+  <div class="Kommentare">
+    <h4>Kommentare</h4>
+  <ul>
+    <?php
+    foreach($Comments as $Comment){
+      echo '<li><p>'.$Comment['content'].'</p><small>'.date('d.m.Y',$Comment['datetime']).'</small>';
+    }
+    ?>
+  </ul>
+  <div class="addComment">
+    <label for="commentTitle">Titel</label>
+    <input type="text" id="commentTitle" name="title"><br/>
+    <label for="commentContent">Kommentar</label>
+    <input type="text" id="commentCreate" name="content">
+  </div>
+  <div class="btnComment">
+    <button type="submit" class="btn btn-primary">Kommentar erfassen</button>
+  </div>
+</form>
