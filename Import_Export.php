955d051b016f3b28deccce2ee66958e868b6375b
Justinio99 <31925156+Justinio99@users.noreply.github.com>
2017-11-23 10:48:27 +0100

    dsfdsf
    dsfdsf

--- Import_Export.php
+++ Import_Export.php
@@ -1,14 +1,53 @@
 <?php
-$row = 1;
-if (($handle = fopen("./import.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num Felder in Zeile $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
+
+
+    $csv_datei = "exchange/import.csv";
+
+    $feler_trenner = ";";
+
+    $zeilen_trenner = "n";
+
+    if (@file_exists($csv_datei) == false) {
+
+        echo 'Die CSV Datei: '. $csv_datei.' gibt es
+            nicht!';
+    } else {
+
+        $datei_inhalt = @file_get_contents($csv_datei);
+
+        $zeilen = explode($zeilen_trenner,
+            $datei_inhalt);
+
+        $anzahl_zeilen = count($zeilen);
+
+        echo 'Es wurden in der CSV Datei: '.
+            $csv_datei.' insgesamt
+            '.($anzahl_zeilen-1).' Zeilen gefunden.<br>';
+
+        if (is_array($zeilen) == true) {
+
+            foreach($zeilen as $zeile) {
+
+                $felder = explode($feler_trenner,
+                    $zeile);
+
+                $i = 0;
+                if (is_array($felder) == true) {
+                    foreach($felder as $felde) {
+
+                        if ($felde != '') {
+
+                            echo (($i != 0) ? ', ':
+                                ''). str_replace('"',
+                                '', $felde);
+
+                            $i++;
+                        }
+                    }
+                }
+
+                echo '<br>';
+            }
         }
     }
-    fclose($handle);
-}
 ?>
