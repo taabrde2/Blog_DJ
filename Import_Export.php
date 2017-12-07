644233aaac2123fce7553185442d3d8f34d9dca0
Justinio99 <31925156+Justinio99@users.noreply.github.com>
2017-12-07 11:26:55 +0100

    upload

--- Import_Export.php
+++ Import_Export.php
@@ -1,10 +1,10 @@
-<button style="width:100px; height:100px;" action="uploadToServer.php" type="button" name="button"></button>
+<button style="width:100px; height:100px;"  action="<?php echo $_SERVER['PHP_SELF']."?function=uploadToServer&bid=".$blogId;?>" type="button" name="button"></button>
 
 
 
 
 
- <form action="upload.php" method="post" enctype="multipart/form-data">
+ <form  action="<?php echo $_SERVER['PHP_SELF']."?function=upload&bid=".$blogId;?>" method="post" enctype="multipart/form-data">
  <input type="file" name="datei"><br>
  <input type="submit" value="Hochladen">
  </form>
