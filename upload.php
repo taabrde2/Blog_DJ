9cdf8560326d6eebe63ea1285eeb40f25d7d74e0
taabrde2 <31621723+taabrde2@users.noreply.github.com>
2017-12-14 10:48:43 +0100

    Abschluss

--- upload.php
+++ upload.php
@@ -6,10 +6,12 @@
  //Pfad zum Upload
  $new_path = $upload_folder.$filename.'.'.$extension;
 
+if($extension == 'csv'){
+  //Dateinamen ersetzen
+ if(file_exists($new_path)) unlink($new_path);
+  //Alles okay, verschiebe Datei an neuen Pfad
+  move_uploaded_file($_FILES['datei']['tmp_name'], $new_path);
+   header('Location: index.php?function=Import_Export&bid='.$blogId);
+}
 
- //Dateinamen ersetzen
-if(file_exists($new_path)) unlink($new_path);
- //Alles okay, verschiebe Datei an neuen Pfad
- move_uploaded_file($_FILES['datei']['tmp_name'], $new_path);
-  header('Location: index.php?function=Import_Export&bid='.$blogId);
  ?>
